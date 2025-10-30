8c2107a76f29a76f0fa3c3845c77808420fa186c
Noelle-Stark <jstark@rocketsoftware.com>
2025-10-30 11:48:55 -0500

    further work on user request validation

--- src/request/Request.py
+++ src/request/Request.py
@@ -43,49 +43,55 @@ class Request(ABC):
             return False
         else:
             return True
-        
-    def validate_str_field(self, field_name, char_min = 0, char_max = 100):
-        if self.field_exists(field_name):
-            if len(self.lic_data[field_name]) > char_max or len(self.lic_data[field_name]) < char_min:
-                raise ValueError(field_name + " field invalid")
-        return True
     
 
     def set_lic_name(self, new_field):
-            validate_field.validate_lic_name((self.lic_data[self.lic_name_key]))
+            validate_field.validate_lic_name(str(new_field))
+            self.lic_name = new_field
     
     def set_lic_version(self, new_field):
-            validate_field.validate_lic_version(str(self.lic_data[self.lic_version_key])) 
+            validate_field.validate_lic_version(str(new_field)) 
+            self.lic_name = new_field
 
     def set_lic_type(self, new_field):
-            validate_field.validate_lic_type(str(self.lic_data[self.lic_type_key]))
+            validate_field.validate_lic_type(str(new_field))
+            self.lic_name = new_field
     
     def set_lic_cost(self, new_field):
-            validate_field.validate_lic_cost(str(self.lic_data[self.lic_cost_key]))
+            validate_field.validate_lic_cost(str(new_field))
+            self.lic_name = new_field
     
     def set_lic_curr(self, new_field):
-            validate_field.validate_lic_curr(str(self.lic_data[self.lic_curr_key]))
+            validate_field.validate_lic_curr(str(new_field))
+            self.lic_name = new_field
     
     def set_lic_pay_period(self, new_field):
-            validate_field.validate_lic_pay_period(str(self.lic_data[self.lic_pay_period_key]))
+            validate_field.validate_lic_pay_period(str(new_field))
+            self.lic_name = new_field
     
     def set_lic_date_of_renewal(self, new_field):
-            validate_field.validate_lic_date_of_renewal(str(self.lic_data[self.lic_date_of_renewal_key]))
+            validate_field.validate_lic_date_of_renewal(str(new_field))
+            self.lic_name = new_field
     
     def set_lic_date_of_expiration(self, new_field):
-            validate_field.validate_lic_date_of_expiration(str(self.lic_data[self.lic_date_of_expiration_key]))
+            validate_field.validate_lic_date_of_expiration(str(new_field))
+            self.lic_name = new_field
     
     def set_lic_restrictions(self, new_field):
-            validate_field.validate_lic_restrictions(str(self.lic_data[self.lic_restrictions_key]))
+            validate_field.validate_lic_restrictions(str(new_field))
+            self.lic_name = new_field
     
     def set_lic_id(self, new_field):
-            validate_field.validate_lic_id(str(self.lic_data[self.lic_id_key]))
+            validate_field.validate_lic_id(str(new_field))
+            self.lic_name = new_field
     
     def set_lic_employee_id(self, new_field):
-            validate_field.validate_lic_employee_id(str(self.lic_data[self.lic_employee_id_key]))
+            validate_field.validate_lic_employee_id(str(new_field))
+            self.lic_name = new_field
     
     def set_lic_computer_id(self, new_field):
-            validate_field.validate_lic_computer_id(str(self.lic_data[self.lic_computer_id_key]))
+            validate_field.validate_lic_computer_id(str(new_field))
+            self.lic_name = new_field
 
 
     @abstractmethod
@@ -115,11 +121,17 @@ class AddLicReq(Request):
 
 class AssignLicense(Request):
     def validate_data(self):
+        if not self.field_exists(self.lic_id_key) or (not self.field_exists(self.lic_employee_id_key) and not self.field_exists(self.lic_computer_id_key)):
+            raise ValueError("Missing one or more required field")
+        elif self.field_exists(self.lic_employee_id_key) and self.field_exists(self.lic_computer_id_key): 
+             raise ValueError("Can't assign employee and computer at same time")
+
+class AssignLicense(Request):
+    def validate_data(self):
         if not self.field_exists(self.lic_id_key):
             pass
 
 
 def testFunc():
-    request_obj = AddLicReq('{"name":"softwareeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee","ver":"4.0","type":"enterprise"}')
+    request_obj = AddLicReq('{"name":"softwareeeeeeeeeee","ver":"4.0","type":"enterprise"}')
     print("all good")
-    validation_checks.is_positive(2)

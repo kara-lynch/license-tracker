73b58a568c2fba99bda4e114d21075ee2c43ac4f
Kara <klynch@rocketsoftware.com>
2025-11-04 11:12:59 -0600

    refactored the UserCredentials object to accept a dict

--- credentials_test.py
+++ credentials_test.py
@@ -2,42 +2,59 @@ from src.credentials.credentials_manager import *
 
 def main():
     test_data = [
-        (
-            263,
-            "Ava'); DROP TABLE Employee; --",
-            "Fishbie",
-            "Japan",
-            "Legal",
-            "Manager"
-        ),
-        (
-            264,
-            "Amélie",
-            "Garçon-Beaumont",
-            "Germany",
-            "Sales",
-            "Sales Agent"
-        ),
-        (
-            265,
-            "Jens",
-            "van der Meer",
-            "Germany",
-            "Information Technology",
-            "Developer"
-        )
+        { #test 1
+            "id": 263,
+            "fName": "Ava'); DROP TABLE Employee; --",
+            "lName": "Fishbie",
+            "loc": "Japan",
+            "dept": "Legal",
+            "title": "Manager"
+        },
+        { #test 2
+            "id": 264,
+            "fName": "Amélie",
+            "lName": "Garçon-Beaumont",
+            "loc": "Germany",
+            "dept": "Sales",
+            "title": "Sales Agent"
+        },
+        { #test 3
+            "id": 265,
+            "fName": "Jens",
+            "lName": "van der Meer",
+            "loc": "Germany",
+            "dept": "Information Technology",
+            "title": "Developer"
+        },
+        {}, #test 4
+        { #test 5
+            "id": 266,
+            "first_name": "Kara",
+            "lName": "Lynch",
+            "loc": "United States",
+            "dept": "Information Technology",
+            "title": "Developer"
+        },
+        { #test 6
+            "id": 267,
+            "fName": "Noelle",
+            "lName": "Stark",
+            "loc": "United States",
+            "dept": "Information Technology"
+        }
     ]
     users = []
+    test_count = 0
     for test in test_data:
-        new_user = UserCredentials(test[0], test[1], test[2], test[3], test[4], test[5])
-
+        test_count += 1
         try:
+            new_user = UserCredentials(test)
             new_user.validate()
             users.append(new_user)
             print(f"{new_user.name()} added successfully")
         except Exception as e:
-            print(f"Validation failed for {new_user.name()}: {e.args[0]}")
-            
+            print(f"Test {test_count} failed for reason: {e.args[0]}")
+
     print("List of users:")
     for user in users:
         print(user.name())
